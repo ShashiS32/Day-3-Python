1da11fae35dc07cb80d361f38b1eb76f557f2e66
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-27 18:17:50 +0000

    Added a possibility of a free ticket if user is 45-55

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ if height >= 120:
   print("You are tall enough to ride the rollercoaster!")
   photo = input("Would you like a photo taken during your ride [extra 3$] ? (y/n): ")
   age = int(input("What is your age?: "))
-  if age >= 18:
+  if age >= 45 and age <= 55:
+    print ("Your ticket is free!")
+  elif age >= 18:
     if photo == "y":
       print ("You have to pay 18$")
     else:
@@ -21,6 +23,9 @@ if height >= 120:
       print ("You have to pay 8$")
     else:
       print("You have to pay 5$")
+
+      
+      
   
 else:
   print("You are not tall enough to ride the rollercoaster!")
