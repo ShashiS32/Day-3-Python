f17868a6ee74885eec24821234197f1841605ecf
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-27 16:15:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print ("Hello welcome to the rollerocatser!")
+height = int(input("What is your height in centimeres?: "))
+
+if height == 120:
+  print("You are tall enough to ride the rollercoaster!")
+
+else:
+  print("You are not tall enough to ride the rollercoaster!")
+
