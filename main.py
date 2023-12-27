dd3d9d78b5cbffa6d1212eb2c5cfb9630a310c7e
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-27 17:51:56 +0000

    added a third condition of if the user wants a photo during the ride

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ print ("Hello welcome to the rollerocatser!")
 height = int(input("What is your height in centimeres?: "))
 
 if height >= 120:
+  
   print("You are tall enough to ride the rollercoaster!")
+  photo = input("Would you like a photo taken during your ride [extra 3$] ? (y/n): ")
   age = int(input("What is your age?: "))
   if age >= 18:
-    print("You have to pay 15$")
+    if photo == "y":
+      print ("You have to pay 18$")
+    else:
+      print("You have to pay 15$")
   elif 12 <= age <= 17:
-    print("You have to pay 10$")
+    if photo == "y":
+      print ("You have to pay 13$")
+    else:
+      print("You have to pay 10$")
   else:
-    print("You have to pay 5$")
-    
+    if photo == "y":
+      print ("You have to pay 8$")
+    else:
+      print("You have to pay 5$")
+  
 else:
   print("You are not tall enough to ride the rollercoaster!")
 
