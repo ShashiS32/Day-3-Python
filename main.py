f4a8762b5468e392410c1a94ccfc7130d4282688
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-27 16:34:51 +0000

    Added an Age condition

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ height = int(input("What is your height in centimeres?: "))
 
 if height >= 120:
   print("You are tall enough to ride the rollercoaster!")
-
+  age = int(input("What is your age?: "))
+  if age >= 18:
+    print("You have to pay 15$")
+  else:
+    print("You have to pay 10$")
 else:
   print("You are not tall enough to ride the rollercoaster!")
 
